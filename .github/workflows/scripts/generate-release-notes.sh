8de5db7a3ed04f8a6baf86148fa9b199bccb649a
Tine Kondo <kondotine@gmail.com>
2025-10-09 09:23:25 +0000

    fix: include the latest changelog in the `GitHub Release`'s  body

--- .github/workflows/scripts/generate-release-notes.sh
+++ .github/workflows/scripts/generate-release-notes.sh
@@ -30,6 +30,10 @@ fi
 cat > release_notes.md << EOF
 This is the latest set of releases that you can use with your agent of choice. We recommend using the Specify CLI to scaffold your projects, however you can download these independently and manage them yourself.
 
+## Changelog
+
+$COMMITS
+
 EOF
 
 echo "Generated release notes:"
